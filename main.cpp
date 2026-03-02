acf693da101b1e00c45bab0a7d94c7bf931a8f97
szabolcsszigetvari2003-prog <szabolcsszigetvari2003@gmail.com>
2026-03-02 16:13:03 +0100

    a jegyzetelt hibák javitasa

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Hibás konstansnév 
-    std::cout << '1-100 ertekek duplazasa' //Hiányzik a sort záró ';', és a szöveg nem a megfelelő jelek közt van
-    for (int i = 0;) //Nincs megadva, hogy meddig, és mekkora lépésekben haladjona ciklus
+    long *b = new long[N_ELEMENTS]; //Hibás konstansnév, az int nem elég nagy
+    std::cout << "1-100 ertekek duplazasa"; //Hiányzik a sort záró ';', és a szöveg nem a megfelelő jelek közt van
+    for (int i = 0;i > b, i++) //Nincs megadva, hogy meddig, és mekkora lépésekben haladjona ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //Nincs megdva meddig menjen a ciklus
+    for (int i = 0; i > *b; i++) //Nincs megdva meddig menjen a ciklus
     {
-        std::cout << "Ertek:" //Nem irja ki az értéket, és hiányzik a parancs végét jező ';'
+         std::cout << "Ertek: " << b[i]; //Nem irja ki az értéket, és hiányzik a parancs végét jező ';'
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //a változo nem kapott értéket
-    for (int i = 0; i < N_ELEMENTS, i++) // ','  a ';' helyett a ciklusban
+    int atlag = 0; //a változo nem kapott értéket
+    for (int i = 0; i < N_ELEMENTS; i++) // ','  a ';' helyett a ciklusban
     {
-        atlag += b[i] //hiányzó ';'
+        atlag += b[i]; //hiányzó ';'
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    delete[] b; //lefoglalt memoria tisztitasa
     return 0;
 }
