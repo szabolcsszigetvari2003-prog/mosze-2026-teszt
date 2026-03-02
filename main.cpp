57a8be4cc438bb32415000a57c3406dfa263725f
szabolcsszigetvari2003-prog <szabolcsszigetvari2003@gmail.com>
2026-03-02 16:02:18 +0100

    hibák jegyzetelése a  main.cpp-ben

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Hibás konstansnév 
+    std::cout << '1-100 ertekek duplazasa' //Hiányzik a sort záró ';', és a szöveg nem a megfelelő jelek közt van
+    for (int i = 0;) //Nincs megadva, hogy meddig, és mekkora lépésekben haladjona ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //Nincs megdva meddig menjen a ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Nem irja ki az értéket, és hiányzik a parancs végét jező ';'
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //a változo nem kapott értéket
+    for (int i = 0; i < N_ELEMENTS, i++) // ','  a ';' helyett a ciklusban
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó ';'
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
